9c0777346811f16b6b9d1f6a7567a2bc8e1845a6
orangisraelwoi <rezin2126@gmail.com>
2026-07-16 21:43:39 +0700

    Auto upload app/src/main/java/com/library/app/MainActivity.java

---
+++ app/src/main/java/com/library/app/MainActivity.java
@@ -0,0 +1,20 @@
+package com.library.app;
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+public class MainActivity extends Activity {
+private WebView myWebView;
+@Override
+protected void onCreate(Bundle savedInstanceState) {
+super.onCreate(savedInstanceState);
+myWebView = new WebView(this);
+WebSettings ws = myWebView.getSettings();
+ws.setJavaScriptEnabled(true);
+ws.setDomStorageEnabled(true);
+myWebView.setWebViewClient(new WebViewClient());
+myWebView.loadUrl("file:///android_asset/www/index.html");
+setContentView(myWebView);
+}
+}
