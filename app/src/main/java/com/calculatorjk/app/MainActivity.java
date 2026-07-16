3aa8b198636e67f64f0c14fde360d50063c254b4
orangisraelwoi <rezin2126@gmail.com>
2026-07-16 21:22:16 +0700

    Auto deployment of app/src/main/java/com/calculatorjk/app/MainActivity.java

---
+++ app/src/main/java/com/calculatorjk/app/MainActivity.java
@@ -0,0 +1,25 @@
+package com.calculatorjk.app;
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends Activity {
+    private WebView myWebView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        myWebView = new WebView(this);
+        WebSettings webSettings = myWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setDomStorageEnabled(true);
+        myWebView.setWebViewClient(new WebViewClient());
+        myWebView.loadUrl("file:///android_asset/www/index.html");
+        setContentView(myWebView);
+    }
+    @Override
+    public void onBackPressed() {
+        if (myWebView.canGoBack()) { myWebView.goBack(); } else { super.onBackPressed(); }
+    }
+}
