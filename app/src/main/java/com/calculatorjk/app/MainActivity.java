e26b39d1661997b15975e9cc8f2190a919d0d876
orangisraelwoi <rezin2126@gmail.com>
2026-07-16 21:28:02 +0700

    Auto upload app/src/main/java/com/calculatorjk/app/MainActivity.java

--- app/src/main/java/com/calculatorjk/app/MainActivity.java
+++ app/src/main/java/com/calculatorjk/app/MainActivity.java
@@ -4,22 +4,17 @@ import android.os.Bundle;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
-
 public class MainActivity extends Activity {
-    private WebView myWebView;
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        myWebView = new WebView(this);
-        WebSettings webSettings = myWebView.getSettings();
-        webSettings.setJavaScriptEnabled(true);
-        webSettings.setDomStorageEnabled(true);
-        myWebView.setWebViewClient(new WebViewClient());
-        myWebView.loadUrl("file:///android_asset/www/index.html");
-        setContentView(myWebView);
-    }
-    @Override
-    public void onBackPressed() {
-        if (myWebView.canGoBack()) { myWebView.goBack(); } else { super.onBackPressed(); }
-    }
+private WebView myWebView;
+@Override
+protected void onCreate(Bundle savedInstanceState) {
+super.onCreate(savedInstanceState);
+myWebView = new WebView(this);
+WebSettings ws = myWebView.getSettings();
+ws.setJavaScriptEnabled(true);
+ws.setDomStorageEnabled(true);
+myWebView.setWebViewClient(new WebViewClient());
+myWebView.loadUrl("file:///android_asset/www/index.html");
+setContentView(myWebView);
+}
 }
